abbc4e0f39f546696ee2b442f02c2e5406e2a0af
Yum-git <yzk.yuma@gmail.com>
2020-07-19 21:59:24 +0900

    gps add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import DeckGL from '@deck.gl/react';
 
+import {IconLayer} from '@deck.gl/layers'
 import {HexagonLayer} from '@deck.gl/aggregation-layers';
 import {StaticMap} from 'react-map-gl';
 
 import * as d3 from 'd3-request';
 
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoieXVtbmlrb25pa28iLCJhIjoiY2s2MzdhczF6MDdlODNtbjB3NzkydzJ6NSJ9.xdMakcrqSAWMmZMYnQiEYw';
+
+const ICON_MAPPING = {
+    marker: {
+        x: 0, 
+        y: 0,
+        width: 256,
+        height: 256,
+        mask: true
+    }
+};
+
+function GPSFunction(){
+
+    navigator.geolocation.getCurrentPosition(
+        function(position){
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            
+            GPSData['coordinates'] = [lat, lon];
+        }
+    );
+};
 
 function DataFunction(){
     return new Promise((resolve, reject) => {
@@ -17,21 +40,45 @@ function DataFunction(){
     });
 };
 const INITIAL_VIEW_STATE = {
-    longitude: 137.335724850895,
-    latitude: 34.8222045598071,
-    zoom: 13,
+    longitude: 136.897874,
+    latitude: 35.1620737,
+    zoom: 10,
     pitch: 45,
     bearing: 0
 };
 
+const colorRange = [
+    [1, 152, 189],
+    [73, 227, 206],
+    [216, 254, 181],
+    [254, 237, 177],
+    [254, 173, 84],
+    [209, 55, 78]
+];
+
 const data = DataFunction().then();
+const GPSData = [
+    {name: 'Present_location', coordinates: [137.335724850895,34.8222045598071]},
+];
 
 class App extends React.Component{
     render(){
+        console.log(GPSData);
         const layers = [
+            new IconLayer({
+                id: 'icon-layer',
+                iconAtlas: 'icon.png',
+                data: GPSData,
+                iconMapping: ICON_MAPPING,
+                getIcon: d => 'marker',
+                sizeScale: 15,
+
+                getPosition: d => d.coordinates,
+            }),
             new HexagonLayer({
                 id: 'hexagon-layer',
                 data: data,
+                colorRange: colorRange,
                 extruded: true,
                 elevationScale: 15,
                 radius: 200,
