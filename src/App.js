00e537caec271815745d9c4e02ba9958fbc144b4
Yum-git <yzk.yuma@gmail.com>
2020-07-25 20:09:50 +0900

    gps plus commmit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import DeckGL from '@deck.gl/react';
 
-import {IconLayer} from '@deck.gl/layers'
+import {IconLayer, GeoJsonLayer} from '@deck.gl/layers'
 import {HexagonLayer} from '@deck.gl/aggregation-layers';
 import {StaticMap} from 'react-map-gl';
 
@@ -9,26 +9,16 @@ import * as d3 from 'd3-request';
 
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoieXVtbmlrb25pa28iLCJhIjoiY2s2MzdhczF6MDdlODNtbjB3NzkydzJ6NSJ9.xdMakcrqSAWMmZMYnQiEYw';
 
+const Hinan_Data_URL = "Hinan_data.geojson";
+
 const ICON_MAPPING = {
     marker: {
         x: 0, 
         y: 0,
-        width: 256,
-        height: 256,
+        width: 64,
+        height: 64,
         mask: true
     }
-};
-
-function GPSFunction(){
-
-    navigator.geolocation.getCurrentPosition(
-        function(position){
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;
-            
-            GPSData['coordinates'] = [lat, lon];
-        }
-    );
 };
 
 function DataFunction(){
@@ -39,6 +29,8 @@ function DataFunction(){
         })
     });
 };
+
+
 const INITIAL_VIEW_STATE = {
     longitude: 136.897874,
     latitude: 35.1620737,
@@ -57,23 +49,58 @@ const colorRange = [
 ];
 
 const data = DataFunction().then();
-const GPSData = [
-    {name: 'Present_location', coordinates: [137.335724850895,34.8222045598071]},
-];
 
 class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            location: null,
+            statein: false
+        }
+    }
+    async GPSFunction(){
+        const location = await this.GPSGet();
+
+        await this.GPSSet(location);
+    };
+
+    GPSGet(timeout = 5000){
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition((position) => {
+                resolve([position.coords.longitude,position.coords.latitude]);
+            }, 
+            reject, 
+            {
+                timeout,
+                enableHighAccuracy: true,
+            });
+        });
+    }
+
+    GPSSet(location){
+        this.setState({
+            location:location,
+            statein:true
+        })
+    }
+
     render(){
-        console.log(GPSData);
+        if(this.state.statein === false){
+            this.GPSFunction();
+        }
+        //console.log(Hinan_Data_URL);
         const layers = [
             new IconLayer({
                 id: 'icon-layer',
+                data: this.state.location,
+                pickable: true,
                 iconAtlas: 'icon.png',
-                data: GPSData,
                 iconMapping: ICON_MAPPING,
                 getIcon: d => 'marker',
                 sizeScale: 15,
-
-                getPosition: d => d.coordinates,
+                getPosition: this.state.location,
+                getSize: d => 5,
+                getColor: d => [Math.sqrt(d.exits), 140, 0]
             }),
             new HexagonLayer({
                 id: 'hexagon-layer',
@@ -83,9 +110,17 @@ class App extends React.Component{
                 elevationScale: 15,
                 radius: 200,
                 getPosition: d => d,
+            }),
+            new GeoJsonLayer({
+                id: 'geo-layer',
+                data: Hinan_Data_URL,
+                pickable: true,
+                stroled: true,
+                extruded: true,
+                visible: true,
+                pointRadiusScale: 1
             })
         ];
-
         return(
             <DeckGL
                 initialViewState={INITIAL_VIEW_STATE}
